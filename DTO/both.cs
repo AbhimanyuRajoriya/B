d55f5ee78c73ebfcdb2647b8d97bf90a53c75ce0
AbhimanyuRajoriya <abhimanyusaini294@gmail.com>
2026-05-18 12:17:45 +0530

    modified:   DTO/both.cs

--- DTO/both.cs
+++ DTO/both.cs
@@ -9,3 +9,6 @@ namespace Bank.wtdDto{
         public int wdt {get; set;}
     }
 }
+
+//DTO means Data Transfer Object
+//DTO is used because the input of withdrawl and deposit are not integer, they will come in JSON
